40f37b8f87d696ab5dca928eb3329a42938c779f
Glitch (pebble-befitting-grill) <none>
2020-08-16 00:27:47 +0000

    Completed Challenge 🍠🐚 Updated with Glitch

--- public/translator.js
+++ public/translator.js
@@ -1,7 +1,176 @@
-import { americanOnly } from './american-only.js';
-import { britishOnly } from './british-only.js';
-import { americanToBritishSpelling } from './american-to-british-spelling.js';
-import { americanToBritishTitles } from './american-to-british-titles.js';
+import { americanOnly } from "./american-only.js";
+import { britishOnly } from "./british-only.js";
+import { americanToBritishSpelling } from "./american-to-british-spelling.js";
+import { americanToBritishTitles } from "./american-to-british-titles.js";
+
+const britishToAmericanSpelling = {};
+Object.keys(americanToBritishSpelling).forEach(key => {
+  britishToAmericanSpelling[americanToBritishSpelling[key]] = key;
+});
+
+const amtToBrt = word => {
+  if (/^\d{1,2}\:\d{1,2}/.test(word)) {
+    return word.replace(":", ".");
+  }
+  return null;
+};
+
+const brtToAmt = word => {
+  if (/^\d{1,2}\.\d{1,2}/.test(word)) {
+    return word.replace(/\./, ":");
+  }
+  return null;
+};
+
+const abPunc = str => {
+  const word = str.replace(/\W/g, "");
+  const word1 = americanToBritishSpelling[word.toLowerCase()] || word;
+  if (word1 === word) {
+    return null;
+  }
+
+  return str.replace(/\w*/, word1);
+};
+
+const baPunc = str => {
+  const word = str.replace(/\W/g, "");
+  const word1 = britishToAmericanSpelling[word.toLowerCase()] || word;
+  if (word1 === word) {
+    return null;
+  }
+
+  return str.replace(/\w*/, word1);
+};
+
+const ameToBreWithWrapper = (ame, l, r) => {
+  let inter = ame
+    .split(" ")
+    .map(word => {
+      const word1 = amtToBrt(word) || abPunc(word);
+      if (word1) {
+        return l + word1 + r;
+      }
+      return word;
+    })
+    .join(" ");
+  Object.keys(americanOnly).forEach(key => {
+    if (new RegExp(key + "\\W", "i").test(inter)) {
+      inter = inter.replace(
+        new RegExp(key, "ig"),
+        "" + l + americanOnly[key] + r
+      );
+    }
+  });
+  Object.keys(americanToBritishTitles).forEach(key => {
+    if (new RegExp(americanToBritishTitles[key] + "\\.\\W", "i").test(inter)) {
+      inter = inter.replace(
+        new RegExp("(?=" + americanToBritishTitles[key] + "\\.)", "ig"),
+        l
+      );
+
+      inter = inter.replace(
+        new RegExp("(?<=" + americanToBritishTitles[key] + ")\\.", "ig"),
+        r
+      );
+    }
+  });
+  console.log(inter);
+  return inter;
+};
+
+const ameToBre = ame => {
+  return ameToBreWithWrapper(ame, "", "");
+};
+
+const breToAmeWithWrapper = (bre, l, r) => {
+  let inter = bre
+    .split(" ")
+    .map(word => {
+      const word1 = brtToAmt(word) || baPunc(word);
+      if (word1) {
+        return l + word1 + r;
+      }
+      return word;
+    })
+    .join(" ");
+  Object.keys(britishOnly)
+    .filter(key => new RegExp(key + "\\W", "i").test(inter))
+    .forEach(key => {
+      inter = inter.replace(
+        new RegExp(key, "ig"),
+        "" + l + britishOnly[key] + r
+      );
+    });
+  Object.keys(americanToBritishTitles)
+    .filter(key =>
+      new RegExp(americanToBritishTitles[key] + "\\W", "i").test(inter)
+    )
+    .forEach(key => {
+      inter = inter.replace(
+        new RegExp("(?=" + americanToBritishTitles[key] + ")", "ig"),
+        l
+      );
+      inter = inter.replace(
+        new RegExp("(?<=" + americanToBritishTitles[key] + ")", "ig"),
+        "." + r
+      );
+    });
+  console.log(inter);
+  return inter;
+};
+
+const breToAme = bre => {
+  return breToAmeWithWrapper(bre, "", "");
+};
+
+const clickTranslate = () => {
+  const option = document.getElementById("locale-select").value;
+
+  const original = document.getElementById("text-input").value;
+  
+  if (!original) {
+    document.getElementById("error-msg").innerHTML = "Error: No text to translate.";
+    return;
+  }
+  
+  let translated = "";
+
+  switch (option) {
+    case "american-to-british":
+      translated = ameToBreWithWrapper(
+        original,
+        '<span class="highlight">',
+        "</span>"
+      );
+
+      break;
+    case "british-to-american":
+      translated = breToAmeWithWrapper(
+        original,
+        '<span class="highlight">',
+        "</span>"
+      );
+      break;
+    default:
+      return;
+  }
+  if (translated === original) {
+    document.getElementById("translated-sentence").innerHTML += "Everything looks good to me!<br>";
+    return;
+  }
+
+  document.getElementById("translated-sentence").innerHTML += translated;
+  document.getElementById("translated-sentence").innerHTML += "<br>";
+};
+
+const clickClear = () => {
+  document.getElementById('text-input').value = '';
+  document.getElementById('translated-sentence').innerHTML = '';
+  document.getElementById('error-msg').innerHTML = '';
+}
+
+document.getElementById("translate-btn").onclick = clickTranslate;
+document.getElementById('clear-btn').onclick = clickClear;
 
 /* 
   Export your functions for testing in Node.
@@ -10,6 +179,9 @@ import { americanToBritishTitles } from './american-to-british-titles.js';
 */
 try {
   module.exports = {
-
-  }
+    ameToBre,
+    breToAme,
+    clickTranslate,
+    clickClear
+  };
 } catch (e) {}
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,26 +6,38 @@
  *       (if additional are added, keep them at the very end!)
  */
 
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
+const { JSDOM } = require("jsdom");
 let Translator;
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {
   suiteSetup(() => {
-    // DOM already mocked -- load translator then run tests
+    
     Translator = require('../public/translator.js');
   });
 
-  suite('Function ____()', () => {
+  suite("Function ____()", () => {
     /* 
       The translated sentence is appended to the `translated-sentence` `div`
       and the translated words or terms are wrapped in 
       `<span class="highlight">...</span>` tags when the "Translate" button is pressed.
     */
     test("Translation appended to the `translated-sentence` `div`", done => {
+      const value = "Mangoes are my favorite fruit.";
+      const output = "Mangoes are my favourite fruit.";
+      const textArea = global.document.getElementById("text-input");
+      textArea.value = value;
 
-      // done();
+      Translator.clickTranslate();
+
+      const appended = global.document.getElementById("translated-sentence")
+        .textContent;
+      console.log(appended);
+      assert.equal(appended, output);
+
+      done();
     });
 
     /* 
@@ -34,8 +46,20 @@ suite('Functional Tests', () => {
       `translated-sentence` `div` when the "Translate" button is pressed.
     */
     test("'Everything looks good to me!' message appended to the `translated-sentence` `div`", done => {
+      global.document.getElementById("translated-sentence").innerHTML = "";
+      const value = "Nothing";
+      const output = "Everything looks good to me!";
+      const textArea = global.document.getElementById("text-input");
+      textArea.value = value;
 
-      // done();
+      Translator.clickTranslate();
+
+      const appended = global.document.getElementById("translated-sentence")
+        .textContent;
+      console.log(appended);
+      assert.equal(appended, output);
+
+      done();
     });
 
     /* 
@@ -44,22 +68,42 @@ suite('Functional Tests', () => {
       the `error-msg` `div`.
     */
     test("'Error: No text to translate.' message appended to the `translated-sentence` `div`", done => {
+      global.document.getElementById("translated-sentence").innerHTML = "";
+      const value = "";
+      const output = "Error: No text to translate.";
+      const textArea = global.document.getElementById("text-input");
+      textArea.value = value;
 
-      // done();
+      Translator.clickTranslate();
+
+      const appended = global.document.getElementById("error-msg").innerHTML;
+      console.log(appended);
+      assert.equal(appended, output);
+
+      done();
     });
-
   });
 
-  suite('Function ____()', () => {
+  suite("Function ____()", () => {
     /* 
       The text area and both the `translated-sentence` and `error-msg`
       `divs` are cleared when the "Clear" button is pressed.
     */
     test("Text area, `translated-sentence`, and `error-msg` are cleared", done => {
-
-      // done();
+      const value = "Mangoes are my favorite fruit.";
+      const output = "Mangoes are my favourite fruit.";
+      const textArea = global.document.getElementById("text-input");
+      textArea.value = value;
+      
+      
+      Translator.clickTranslate();
+      Translator.clickClear();
+      
+      assert.equal(global.document.getElementById("error-msg").innerHTML, '');
+      assert.equal(global.document.getElementById("translated-sentence").innerHTML, '');
+      assert.equal(global.document.getElementById("text-input").value, '');
+      
+      done();
     });
-
   });
-
 });
